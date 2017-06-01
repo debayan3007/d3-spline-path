f541929b37e88242d94a45f63404a1cb45ad3f16
Debayan Singha <debayan@fusioncharts.com>
2017-06-01 18:18:42 +0530

    change in package.json

--- test/spline-test.js
+++ test/spline-test.js
@@ -1,5 +1,5 @@
-var tape = require("tape"),
-    foo = require("../");
+// var tape = require("tape"),
+//     foo = require("../");
 
 // tape("foo() returns the answer to the ultimate question of life, the universe, and everything.", function(test) {
 //   test.equal(foo.foo(), 42);
